d953982d044449959fd20995187fdf0a0ed5d01d
shafikshaon <shafikshaon@gmail.com>
2018-02-28 15:09:23 +0600

    initial project files added

---
+++ Eatm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eatm.MvcApplication" Language="C#" %>
---
+++ Eatm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eatm.Startup))]
+namespace Eatm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
