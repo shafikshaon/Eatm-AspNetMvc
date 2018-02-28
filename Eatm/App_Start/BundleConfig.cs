dfcd3e6ad35f7406d0045d4f2eae991a8bb0d6f2
shafikshaon <shafikshaon@gmail.com>
2018-02-28 15:25:13 +0600

    basic templating

--- Eatm/App_Start/BundleConfig.cs
+++ Eatm/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Eatm
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/journal.css",
                       "~/Content/site.css"));
         }
     }
